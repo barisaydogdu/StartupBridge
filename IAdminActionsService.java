c3918f6c054596beba0a85d6be969581649580c4
Barış Aydoğdu <46091973+barisaydogdu@users.noreply.github.com>
2024-11-09 15:06:17 +0300

    Revert "Controller"

--- IAdminActionsService.java
+++
@@ -1,7 +0,0 @@
-package com.filepackage.service;
-
-import com.filepackage.dto.AdminActionsDto;
-
-public interface IAdminActionsService<AdminActionsDto, Long> extends IBaseService<AdminActionsDto, Long> {
-    AdminActionsDto createAdminAction(AdminActionsDto adminActionsDto);
-}
--- IEducationRepository.java
+++
@@ -1,8 +0,0 @@
-package com.filepackage.repository;
-
-import com.filepackage.entity.Project;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface IEducationRepository extends IEducationRepository<Education>{
-}
